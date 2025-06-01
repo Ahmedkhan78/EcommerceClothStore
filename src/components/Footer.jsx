d86a0beb964b23f495f3245f0ea71f861a6b114b
AhmedHasan <109160297+Ahmedkhan78@users.noreply.github.com>
2025-06-01 16:57:08 +0530

    Final Commit a bit tweak in head for seo and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='bg-gray-950 py-12'>
             <div className='container mx-auto'>
                 <p className='text-white text-center '>
-                    Copyright &copy; Ecommerce Shop 2022. All right reserved.
+                    Copyright &copy; Ecommerce Shop 2025. All right reserved.
                 </p>
             </div>
         </footer>
